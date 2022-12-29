ababb33480b254a5b3b651a95bb4e8a0355ee90a
Juan Pablo Fernandez <juan.pablo.fernandez@outlook.com>
2022-12-28 23:26:58 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import CardWidget from './components/CardWidget';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar/>
+        <NavBar>
+          <CardWidget/>
+        </NavBar>
       </header>
     </div>
   );
---
+++ src/components/CardWidget.jsx
@@ -0,0 +1,8 @@
+
+const CardWidget = () => {
+  return (
+    <img src="https://cdn.icon-icons.com/icons2/2272/PNG/96/pullover_clothing_icon_140720.png" alt="icono" />
+  )
+}
+
+export default CardWidget
